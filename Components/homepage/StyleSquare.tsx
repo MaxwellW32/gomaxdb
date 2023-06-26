321147059f3742d7d23a39d1b306e3438a438c59
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-06-26 05:07:44 -0500

    ya

--- Components/homepage/StyleSquare.tsx
+++ Components/homepage/StyleSquare.tsx
@@ -1,320 +1,329 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-import styles from "./page.module.css";
-import useShapesLookup, { shapesLookup } from "@/utilities/ShapesLookup";
-import startShapes from "@/utilities/StartShapes";
-import ReactPlayer from "react-player/youtube";
-import type { baseReadData } from "@/app/page";
+// "use client";
+// import { useEffect, useRef, useState } from "react";
+// import styles from "./page.module.css";
+// import useShapesLookup, { shapesLookup } from "@/utilities/ShapesLookup";
+// import startShapes from "@/utilities/StartShapes";
+// import ReactPlayer from "react-player/youtube";
+// import type { baseReadData } from "@/app/page";
 
-export default function StyleSquare({
-  id,
-  speed,
-  gravity,
-  shapes,
-  colors,
-  angle,
-  text,
-  username,
-  audioLink,
-  imgLinks,
-  ytLinks,
-  deleteSpecific,
-}: baseReadData & { deleteSpecific: (input: string) => void }) {
-  const seenColors = colors.split("|");
-  const mainBoxRef = useRef<HTMLDivElement>(null!);
-  const ball = useRef<HTMLDivElement>(null!);
+// export default function StyleSquare({
+//   id,
+//   speed,
+//   gravity,
+//   shapes,
+//   colors,
+//   angle,
+//   text,
+//   username,
+//   audioLink,
+//   imgLinks,
+//   ytLinks,
+//   deleteSpecific,
+// }: baseReadData & { deleteSpecific: (input: string) => void }) {
+//   const seenColors = colors.split("|");
+//   const mainBoxRef = useRef<HTMLDivElement>(null!);
+//   const ball = useRef<HTMLDivElement>(null!);
 
-  const [position, setPosition] = useState(0);
-  const ballAnimTime = speed;
+//   const [position, setPosition] = useState(0);
+//   const ballAnimTime = speed;
 
-  const [seenImgLinks, seenImgLinksSet] = useState<string[] | null>(() => {
-    if (imgLinks) {
-      return JSON.parse(imgLinks)
-    } else {
-      return null
-    }
-  })
+//   const [seenImgLinks, seenImgLinksSet] = useState<string[] | null>(() => {
+//     if (imgLinks) {
+//       return JSON.parse(imgLinks)
+//     } else {
+//       return null
+//     }
+//   })
 
-  const [seenYtLinks, seenYtLinksSet] = useState<string[] | null>(() => {
-    if (ytLinks) {
-      return JSON.parse(ytLinks)
-    } else {
-      return null
-    }
-  })
+//   const [seenYtLinks, seenYtLinksSet] = useState<string[] | null>(() => {
+//     if (ytLinks) {
+//       return JSON.parse(ytLinks)
+//     } else {
+//       return null
+//     }
+//   })
 
 
 
-  //animate ball
-  useEffect(() => {
-    let atStart = true;
+//   //animate ball
+//   useEffect(() => {
+//     let atStart = true;
 
-    const animationId = setInterval(() => {
-      if (atStart) {
-        setPosition(mainBoxRef.current.offsetWidth - ball.current.offsetWidth);
-      } else {
-        setPosition(0);
-      }
+//     const animationId = setInterval(() => {
+//       if (atStart) {
+//         setPosition(mainBoxRef.current.offsetWidth - ball.current.offsetWidth);
+//       } else {
+//         setPosition(0);
+//       }
 
-      atStart = !atStart;
-    }, ballAnimTime);
+//       atStart = !atStart;
+//     }, ballAnimTime);
 
-    return () => clearInterval(animationId);
-  }, []);
+//     return () => clearInterval(animationId);
+//   }, []);
 
-  const [amountOfDroplets, amountOfDropletsSet] = useState<number[]>([]);
+//   const [amountOfDroplets, amountOfDropletsSet] = useState<number[]>([]);
 
-  //parse shapes
-  useEffect(() => {
-    const firstLetter = shapes[0];
+//   //parse shapes
+//   useEffect(() => {
+//     const firstLetter = shapes[0];
 
-    let counter = 0;
+//     let counter = 0;
 
-    for (counter; counter < startShapes.length; counter++) {
-      if (firstLetter === startShapes[counter]) {
-        break;
-      }
-    }
+//     for (counter; counter < startShapes.length; counter++) {
+//       if (firstLetter === startShapes[counter]) {
+//         break;
+//       }
+//     }
 
-    counter++;
+//     counter++;
 
-    const newArr: number[] = [];
-    for (let index = 0; index < counter; index++) {
-      newArr.push(index);
-    }
+//     const newArr: number[] = [];
+//     for (let index = 0; index < counter; index++) {
+//       newArr.push(index);
+//     }
 
-    amountOfDropletsSet(newArr);
-  }, []);
+//     amountOfDropletsSet(newArr);
+//   }, []);
 
-  const [parentBoxHeight, parentBoxHeightSet] = useState(0);
+//   const [parentBoxHeight, parentBoxHeightSet] = useState(0);
 
-  useEffect(() => {
-    parentBoxHeightSet(mainBoxRef.current.offsetHeight);
-  }, []);
+//   useEffect(() => {
+//     parentBoxHeightSet(mainBoxRef.current.offsetHeight);
+//   }, []);
 
-  const [currentText, currentTextSet] = useState("");
+//   const [currentText, currentTextSet] = useState("");
 
-  const [mouseHovering, mouseHoveringSet] = useState(false);
+//   const [mouseHovering, mouseHoveringSet] = useState(false);
 
-  const [latestSignInAs, latestSignInAsSet] = useState("");
+//   const [latestSignInAs, latestSignInAsSet] = useState("");
 
-  //latest sign in on mouse hover
-  useEffect(() => {
-    if (mouseHovering) {
-      latestSignInAsSet(() => {
-        let nameSeen = localStorage.getItem("loggedInAs");
-        if (!nameSeen) {
-          nameSeen = "";
-        }
+//   //latest sign in on mouse hover
+//   useEffect(() => {
+//     if (mouseHovering) {
+//       latestSignInAsSet(() => {
+//         let nameSeen = localStorage.getItem("loggedInAs");
+//         if (!nameSeen) {
+//           nameSeen = "";
+//         }
 
-        return nameSeen;
-      });
-    }
-  }, [mouseHovering]);
+//         return nameSeen;
+//       });
+//     }
+//   }, [mouseHovering]);
 
-  const adminSignin = latestSignInAs === "admin";
-  const canClick = username !== latestSignInAs || latestSignInAs.length < 1;
-  const shouldDisa = adminSignin ? false : canClick;
+//   const adminSignin = latestSignInAs === "admin";
+//   const canClick = username !== latestSignInAs || latestSignInAs.length < 1;
+//   const shouldDisa = adminSignin ? false : canClick;
 
-  //display stats
-  useEffect(() => {
-    const newArr = [
-      `speed: ${speed}`,
-      `gravity: ${gravity}`,
-      `shapes: ${shapes}`,
-      `backgroundColors: ${seenColors[0]} + ${seenColors[1]}`,
-      `color rotation: ${angle}`,
-    ];
+//   //display stats
+//   useEffect(() => {
+//     const newArr = [
+//       `speed: ${speed}`,
+//       `gravity: ${gravity}`,
+//       `shapes: ${shapes}`,
+//       `backgroundColors: ${seenColors[0]} + ${seenColors[1]}`,
+//       `color rotation: ${angle}`,
+//     ];
 
-    let arrPos = 0;
+//     let arrPos = 0;
 
-    let myInterv: NodeJS.Timer;
+//     let myInterv: NodeJS.Timer;
 
-    if (mouseHovering && adminSignin) {
-      myInterv = setInterval(() => {
-        currentTextSet(newArr[arrPos]);
-        arrPos++;
-        if (arrPos > newArr.length - 1) {
-          arrPos = 0;
-        }
-      }, 1000);
-    }
+//     if (mouseHovering && adminSignin) {
+//       myInterv = setInterval(() => {
+//         currentTextSet(newArr[arrPos]);
+//         arrPos++;
+//         if (arrPos > newArr.length - 1) {
+//           arrPos = 0;
+//         }
+//       }, 1000);
+//     }
 
-    return () => {
-      currentTextSet("");
+//     return () => {
+//       currentTextSet("");
 
-      return clearInterval(myInterv);
-    };
-  }, [mouseHovering, adminSignin]);
+//       return clearInterval(myInterv);
+//     };
+//   }, [mouseHovering, adminSignin]);
 
-  const [mouseClicked, mouseClickedSet] = useState(false);
+//   const [mouseClicked, mouseClickedSet] = useState(false);
+//   return (
+//     <div
+//       onMouseEnter={() => mouseHoveringSet(true)}
+//       onMouseLeave={() => mouseHoveringSet(false)}
+//       onClick={() => mouseClickedSet((prev) => !prev)}
+//       style={{
+//         background: `linear-gradient(${angle}deg, ${seenColors[0]}, ${seenColors[1]})`,
+//       }}
+//       className={styles.mainBox}
+//       ref={mainBoxRef}
+//     >
+//       <button
+//         className={styles.deleteButton}
+//         style={{
+//           display: !shouldDisa ? "block" : "none",
+//           opacity: mouseHovering ? "1" : "0",
+//         }}
+//         onClick={() => {
+//           deleteSpecific(id);
+//         }}
+//       >
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           height="1em"
+//           viewBox="0 0 512 512"
+//         >
+//           <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+//         </svg>
+//       </button>
+
+//       <div
+//         style={{ pointerEvents: "none", display: "none" }}
+//         onClick={(e) => {
+//           e.stopPropagation();
+//         }}
+//       >
+//         <ReactPlayer
+//           loop={true}
+//           playing={mouseClicked}
+//           url={
+//             audioLink !== null
+//               ? audioLink
+//               : "https://www.youtube.com/watch?v=l9LNIUNa7x4&ab_channel=Tatsumi"
+//           }
+//         />
+//       </div>
+
+//       <div
+//         style={{
+//           translate: `${position}px`,
+//           transition: `translate ${ballAnimTime}ms`,
+//         }}
+//         className={styles.ball}
+//         ref={ball}
+//       ></div>
+
+//       {amountOfDroplets.map((eachItem) => (
+//         <Droplet
+//           key={eachItem}
+//           gravity={gravity}
+//           shapes={shapes}
+//           parentHeight={parentBoxHeight}
+//         />
+//       ))}
+
+//       <div className={styles.contentCont}>
+
+//         <div className={styles.textCont}>
+//           <p className={styles.squareText}>{text}</p>
+
+//           <p className={styles.displayName}>
+//             {username} {username.length > 0 ? "-" : ""}
+//           </p>
+
+//           <p
+//             style={{ display: mouseHovering && adminSignin ? "block" : "none" }}
+//             className={styles.squareTextStats}
+//           >
+//             {currentText}
+//           </p>
+//         </div>
+
+//         {seenImgLinks && <>
+//           {seenImgLinks.map(eachLink => (
+//             <img src={eachLink} />
+//           ))}
+//         </>}
+
+
+//         {seenYtLinks && <>
+//           {seenYtLinks.map(eachLink => (
+//             <ReactPlayer
+//               playing={false}
+//               url={eachLink}
+//             />
+//           ))}
+//         </>}
+
+//       </div>
+
+
+
+//     </div>
+//   );
+// }
+
+// function Droplet({
+//   gravity,
+//   shapes,
+//   parentHeight,
+// }: {
+//   gravity: number;
+//   shapes: string;
+//   parentHeight: number;
+// }) {
+//   const [rndX, rndXSet] = useState(Math.random() * 101);
+//   const [rndShapeSelect] = useState(Math.floor(Math.random() * 2));
+
+//   const dropletRef = useRef<HTMLDivElement>(null!);
+
+//   const {
+//     width: seenWidth,
+//     rotation: seenRotation,
+//     url,
+//   } = useShapesLookup(shapes[rndShapeSelect]);
+
+//   const [initialYPos] = useState(0 - seenWidth);
+//   const [yPos, setYPos] = useState(initialYPos);
+
+//   const [initialTransitionVal] = useState(
+//     gravity - Math.floor(Math.random() * 251)
+//   );
+//   const [transitionVal, setTransitionVal] = useState(initialTransitionVal);
+
+//   useEffect(() => {
+//     let atBottom = false;
+//     const rndTimer = Math.floor(Math.random() * 3001) + 200;
+
+//     setTimeout(() => {
+//       const fallInterval = setInterval(() => {
+//         if (!atBottom) {
+//           setTransitionVal(initialTransitionVal);
+//           setYPos(parentHeight);
+//         } else {
+//           setTransitionVal(0);
+//           setYPos(initialYPos);
+//           rndXSet(Math.random() * 101);
+//         }
+
+//         atBottom = !atBottom;
+//       }, transitionVal);
+//     }, rndTimer);
+//   }, []);
+
+//   return (
+//     <div
+//       style={{
+//         left: `${rndX}%`,
+//         translate: `0 ${yPos}px`,
+//         transition: `translate ${transitionVal}ms linear`,
+//         width: `${seenWidth}px`,
+//         rotate: `${seenRotation}deg`,
+//       }}
+//       className={styles.droplet}
+//       ref={dropletRef}
+//     >
+//       {shapes[rndShapeSelect]}
+//     </div>
+//   );
+// }
+
+
+
+export default async function StyleSquare() {
+
   return (
-    <div
-      onMouseEnter={() => mouseHoveringSet(true)}
-      onMouseLeave={() => mouseHoveringSet(false)}
-      onClick={() => mouseClickedSet((prev) => !prev)}
-      style={{
-        background: `linear-gradient(${angle}deg, ${seenColors[0]}, ${seenColors[1]})`,
-      }}
-      className={styles.mainBox}
-      ref={mainBoxRef}
-    >
-      <button
-        className={styles.deleteButton}
-        style={{
-          display: !shouldDisa ? "block" : "none",
-          opacity: mouseHovering ? "1" : "0",
-        }}
-        onClick={() => {
-          deleteSpecific(id);
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 512 512"
-        >
-          <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
-        </svg>
-      </button>
-
-      <div
-        style={{ pointerEvents: "none", display: "none" }}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-      >
-        <ReactPlayer
-          loop={true}
-          playing={mouseClicked}
-          url={
-            audioLink !== null
-              ? audioLink
-              : "https://www.youtube.com/watch?v=l9LNIUNa7x4&ab_channel=Tatsumi"
-          }
-        />
-      </div>
-
-      <div
-        style={{
-          translate: `${position}px`,
-          transition: `translate ${ballAnimTime}ms`,
-        }}
-        className={styles.ball}
-        ref={ball}
-      ></div>
-
-      {amountOfDroplets.map((eachItem) => (
-        <Droplet
-          key={eachItem}
-          gravity={gravity}
-          shapes={shapes}
-          parentHeight={parentBoxHeight}
-        />
-      ))}
-
-      <div className={styles.contentCont}>
-
-        <div className={styles.textCont}>
-          <p className={styles.squareText}>{text}</p>
-
-          <p className={styles.displayName}>
-            {username} {username.length > 0 ? "-" : ""}
-          </p>
-
-          <p
-            style={{ display: mouseHovering && adminSignin ? "block" : "none" }}
-            className={styles.squareTextStats}
-          >
-            {currentText}
-          </p>
-        </div>
-
-        {seenImgLinks && <>
-          {seenImgLinks.map(eachLink => (
-            <img src={eachLink} />
-          ))}
-        </>}
-
-
-        {seenYtLinks && <>
-          {seenYtLinks.map(eachLink => (
-            <ReactPlayer
-              playing={false}
-              url={eachLink}
-            />
-          ))}
-        </>}
-
-      </div>
-
-
-
-    </div>
-  );
-}
-
-function Droplet({
-  gravity,
-  shapes,
-  parentHeight,
-}: {
-  gravity: number;
-  shapes: string;
-  parentHeight: number;
-}) {
-  const [rndX, rndXSet] = useState(Math.random() * 101);
-  const [rndShapeSelect] = useState(Math.floor(Math.random() * 2));
-
-  const dropletRef = useRef<HTMLDivElement>(null!);
-
-  const {
-    width: seenWidth,
-    rotation: seenRotation,
-    url,
-  } = useShapesLookup(shapes[rndShapeSelect]);
-
-  const [initialYPos] = useState(0 - seenWidth);
-  const [yPos, setYPos] = useState(initialYPos);
-
-  const [initialTransitionVal] = useState(
-    gravity - Math.floor(Math.random() * 251)
-  );
-  const [transitionVal, setTransitionVal] = useState(initialTransitionVal);
-
-  useEffect(() => {
-    let atBottom = false;
-    const rndTimer = Math.floor(Math.random() * 3001) + 200;
-
-    setTimeout(() => {
-      const fallInterval = setInterval(() => {
-        if (!atBottom) {
-          setTransitionVal(initialTransitionVal);
-          setYPos(parentHeight);
-        } else {
-          setTransitionVal(0);
-          setYPos(initialYPos);
-          rndXSet(Math.random() * 101);
-        }
-
-        atBottom = !atBottom;
-      }, transitionVal);
-    }, rndTimer);
-  }, []);
-
-  return (
-    <div
-      style={{
-        left: `${rndX}%`,
-        translate: `0 ${yPos}px`,
-        transition: `translate ${transitionVal}ms linear`,
-        width: `${seenWidth}px`,
-        rotate: `${seenRotation}deg`,
-      }}
-      className={styles.droplet}
-      ref={dropletRef}
-    >
-      {shapes[rndShapeSelect]}
-    </div>
-  );
+    <p>hi there StyleSquare</p>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,127 +1,134 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { PrismaClient } from "@prisma/client";
-import { revalidatePath } from "next/cache";
-import StyleSquare from "@/Components/homepage/StyleSquare";
-import RecordInput from "@/Components/homepage/RecordInput";
-import startShapes from "@/utilities/StartShapes";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+// import { PrismaClient } from "@prisma/client";
+// import { revalidatePath } from "next/cache";
+// import StyleSquare from "@/Components/homepage/StyleSquare";
+// import RecordInput from "@/Components/homepage/RecordInput";
+// import startShapes from "@/utilities/StartShapes";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-interface baseReadData {
-  id: string;
-  createdAt: Date;
-  username: string;
-  speed: number;
-  gravity: number;
-  shapes: string;
-  colors: string;
-  angle: number;
-  text: string;
-  audioLink: string | null;
-  ytLinks: string | null;
-  imgLinks: string | null;
-}
+// interface baseReadData {
+//   id: string;
+//   createdAt: Date;
+//   username: string;
+//   speed: number;
+//   gravity: number;
+//   shapes: string;
+//   colors: string;
+//   angle: number;
+//   text: string;
+//   audioLink: string | null;
+//   ytLinks: string | null;
+//   imgLinks: string | null;
+// }
 
-interface baseSendData extends Omit<baseReadData, "id" | "createdAt"> { }
+// interface baseSendData extends Omit<baseReadData, "id" | "createdAt"> { }
 
-export type { baseReadData, baseSendData };
+// export type { baseReadData, baseSendData };
+
+// export default async function Home() {
+//   let allInfo: baseReadData[] = [];
+
+//   try {
+//     allInfo = await prisma.base.findMany();
+//   } catch (error) {
+//     alert("couldnt fetch");
+//   }
+
+//   async function newRecord(input: baseSendData, usingCustomSett: boolean) {
+//     "use server";
+
+//     let newRecordObj = {} as baseSendData;
+
+//     if (usingCustomSett) {
+//       //validation
+//       const preValidData = { ...input };
+
+//       preValidData.speed = preValidData.speed >= 500 ? preValidData.speed : 500;
+//       preValidData.shapes =
+//         preValidData.shapes && preValidData.shapes.length > 1
+//           ? preValidData.shapes
+//           : "BA";
+//       preValidData.gravity =
+//         preValidData.gravity >= 500 ? preValidData.gravity : 500;
+//       preValidData.angle = preValidData.angle >= 0 ? preValidData.angle : 0;
+
+//       newRecordObj = { ...preValidData };
+//     } else {
+//       newRecordObj = { ...input, ...mkRndBgData() };
+//     }
+
+//     function mkRndBgData() {
+//       const colors = [
+//         "red",
+//         "blue",
+//         "yellow",
+//         "green",
+//         "purple",
+//         "orange",
+//         "pink",
+//       ];
+
+//       const newObj = {
+//         colors:
+//           colors[Math.floor(Math.random() * colors.length)] +
+//           "|" +
+//           colors[Math.floor(Math.random() * colors.length)],
+//         speed: Math.floor(Math.random() * 6000) + 500,
+//         shapes:
+//           startShapes[Math.floor(Math.random() * startShapes.length)] +
+//           startShapes[Math.floor(Math.random() * startShapes.length)],
+//         gravity: Math.floor(Math.random() * 11) * 1000 + 500,
+//         angle: Math.floor(Math.random() * 361),
+//       };
+
+//       return newObj;
+//     }
+
+//     await prisma.base.create({
+//       data: newRecordObj,
+//     });
+
+//     revalidatePath("/");
+//   }
+
+//   async function deleteSpecific(input: string) {
+//     "use server";
+
+//     const seenId = input;
+
+//     await prisma.base.delete({
+//       where: { id: seenId },
+//     });
+//     console.log(`deleted specific ${seenId}`);
+//     revalidatePath("/");
+//   }
+
+//   if (!allInfo) return "not yet found";
+
+//   return (
+//     <main className={styles.mainDiv}>
+//       <h1>Public Notes</h1>
+
+//       <RecordInput newRecord={newRecord} />
+
+//       <div className={styles.squareHolder}>
+//         {allInfo.map((eachRecord) => (
+//           <StyleSquare
+//             key={eachRecord.id}
+//             {...eachRecord}
+//             deleteSpecific={deleteSpecific}
+//           />
+//         ))}
+//       </div>
+//     </main>
+//   );
+// }
 
 export default async function Home() {
-  let allInfo: baseReadData[] = [];
-
-  try {
-    allInfo = await prisma.base.findMany();
-  } catch (error) {
-    alert("couldnt fetch");
-  }
-
-  async function newRecord(input: baseSendData, usingCustomSett: boolean) {
-    "use server";
-
-    let newRecordObj = {} as baseSendData;
-
-    if (usingCustomSett) {
-      //validation
-      const preValidData = { ...input };
-
-      preValidData.speed = preValidData.speed >= 500 ? preValidData.speed : 500;
-      preValidData.shapes =
-        preValidData.shapes && preValidData.shapes.length > 1
-          ? preValidData.shapes
-          : "BA";
-      preValidData.gravity =
-        preValidData.gravity >= 500 ? preValidData.gravity : 500;
-      preValidData.angle = preValidData.angle >= 0 ? preValidData.angle : 0;
-
-      newRecordObj = { ...preValidData };
-    } else {
-      newRecordObj = { ...input, ...mkRndBgData() };
-    }
-
-    function mkRndBgData() {
-      const colors = [
-        "red",
-        "blue",
-        "yellow",
-        "green",
-        "purple",
-        "orange",
-        "pink",
-      ];
-
-      const newObj = {
-        colors:
-          colors[Math.floor(Math.random() * colors.length)] +
-          "|" +
-          colors[Math.floor(Math.random() * colors.length)],
-        speed: Math.floor(Math.random() * 6000) + 500,
-        shapes:
-          startShapes[Math.floor(Math.random() * startShapes.length)] +
-          startShapes[Math.floor(Math.random() * startShapes.length)],
-        gravity: Math.floor(Math.random() * 11) * 1000 + 500,
-        angle: Math.floor(Math.random() * 361),
-      };
-
-      return newObj;
-    }
-
-    await prisma.base.create({
-      data: newRecordObj,
-    });
-
-    revalidatePath("/");
-  }
-
-  async function deleteSpecific(input: string) {
-    "use server";
-
-    const seenId = input;
-
-    await prisma.base.delete({
-      where: { id: seenId },
-    });
-    console.log(`deleted specific ${seenId}`);
-    revalidatePath("/");
-  }
-
-  if (!allInfo) return "not yet found";
 
   return (
-    <main className={styles.mainDiv}>
-      <h1>Public Notes</h1>
-
-      <RecordInput newRecord={newRecord} />
-
-      <div className={styles.squareHolder}>
-        {allInfo.map((eachRecord) => (
-          <StyleSquare
-            key={eachRecord.id}
-            {...eachRecord}
-            deleteSpecific={deleteSpecific}
-          />
-        ))}
-      </div>
-    </main>
-  );
+    <p>hi there</p>
+  )
 }
