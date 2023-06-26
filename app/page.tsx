2161e1dbcf5fd9927189c3d06447704b7c2d77c0
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-06-26 05:32:27 -0500

    q

--- app/page.tsx
+++ app/page.tsx
@@ -36,74 +36,74 @@ export default async function Home() {
     alert("couldnt fetch");
   }
 
-  async function newRecord(input: baseSendData, usingCustomSett: boolean) {
-    "use server";
+  // async function newRecord(input: baseSendData, usingCustomSett: boolean) {
+  //   "use server";
 
-    let newRecordObj = {} as baseSendData;
+  //   let newRecordObj = {} as baseSendData;
 
-    if (usingCustomSett) {
-      //validation
-      const preValidData = { ...input };
+  //   if (usingCustomSett) {
+  //     //validation
+  //     const preValidData = { ...input };
 
-      preValidData.speed = preValidData.speed >= 500 ? preValidData.speed : 500;
-      preValidData.shapes =
-        preValidData.shapes && preValidData.shapes.length > 1
-          ? preValidData.shapes
-          : "BA";
-      preValidData.gravity =
-        preValidData.gravity >= 500 ? preValidData.gravity : 500;
-      preValidData.angle = preValidData.angle >= 0 ? preValidData.angle : 0;
+  //     preValidData.speed = preValidData.speed >= 500 ? preValidData.speed : 500;
+  //     preValidData.shapes =
+  //       preValidData.shapes && preValidData.shapes.length > 1
+  //         ? preValidData.shapes
+  //         : "BA";
+  //     preValidData.gravity =
+  //       preValidData.gravity >= 500 ? preValidData.gravity : 500;
+  //     preValidData.angle = preValidData.angle >= 0 ? preValidData.angle : 0;
 
-      newRecordObj = { ...preValidData };
-    } else {
-      newRecordObj = { ...input, ...mkRndBgData() };
-    }
+  //     newRecordObj = { ...preValidData };
+  //   } else {
+  //     newRecordObj = { ...input, ...mkRndBgData() };
+  //   }
 
-    function mkRndBgData() {
-      const colors = [
-        "red",
-        "blue",
-        "yellow",
-        "green",
-        "purple",
-        "orange",
-        "pink",
-      ];
+  //   function mkRndBgData() {
+  //     const colors = [
+  //       "red",
+  //       "blue",
+  //       "yellow",
+  //       "green",
+  //       "purple",
+  //       "orange",
+  //       "pink",
+  //     ];
 
-      const newObj = {
-        colors:
-          colors[Math.floor(Math.random() * colors.length)] +
-          "|" +
-          colors[Math.floor(Math.random() * colors.length)],
-        speed: Math.floor(Math.random() * 6000) + 500,
-        shapes:
-          startShapes[Math.floor(Math.random() * startShapes.length)] +
-          startShapes[Math.floor(Math.random() * startShapes.length)],
-        gravity: Math.floor(Math.random() * 11) * 1000 + 500,
-        angle: Math.floor(Math.random() * 361),
-      };
+  //     const newObj = {
+  //       colors:
+  //         colors[Math.floor(Math.random() * colors.length)] +
+  //         "|" +
+  //         colors[Math.floor(Math.random() * colors.length)],
+  //       speed: Math.floor(Math.random() * 6000) + 500,
+  //       shapes:
+  //         startShapes[Math.floor(Math.random() * startShapes.length)] +
+  //         startShapes[Math.floor(Math.random() * startShapes.length)],
+  //       gravity: Math.floor(Math.random() * 11) * 1000 + 500,
+  //       angle: Math.floor(Math.random() * 361),
+  //     };
 
-      return newObj;
-    }
+  //     return newObj;
+  //   }
 
-    await prisma.base.create({
-      data: newRecordObj,
-    });
+  //   await prisma.base.create({
+  //     data: newRecordObj,
+  //   });
 
-    revalidatePath("/");
-  }
+  //   revalidatePath("/");
+  // }
 
-  async function deleteSpecific(input: string) {
-    "use server";
+  // async function deleteSpecific(input: string) {
+  //   "use server";
 
-    const seenId = input;
+  //   const seenId = input;
 
-    await prisma.base.delete({
-      where: { id: seenId },
-    });
-    console.log(`deleted specific ${seenId}`);
-    revalidatePath("/");
-  }
+  //   await prisma.base.delete({
+  //     where: { id: seenId },
+  //   });
+  //   console.log(`deleted specific ${seenId}`);
+  //   revalidatePath("/");
+  // }
 
   if (!allInfo) return "not yet found";
 
@@ -120,6 +120,17 @@ export default async function Home() {
           </div>
         ))}
       </div>
+
+
+      {/* <div className={styles.squareHolder}>
+        {allInfo.map((eachRecord) => (
+          <StyleSquare
+            key={eachRecord.id}
+            {...eachRecord}
+            deleteSpecific={deleteSpecific}
+          />
+        ))}
+      </div> */}
     </main>
   );
 }
