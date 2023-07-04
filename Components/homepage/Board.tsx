9a5df5a068964c5af0ef2ad33a9e448bfd833a84
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-07-04 00:38:25 -0500

    cool

--- Components/homepage/Board.tsx
+++ Components/homepage/Board.tsx
@@ -235,9 +235,8 @@ export default function Board({
         </ul>
 
         <svg
-          style={{ fill: moreSettingsClicked ? "#fff" : "#000" }}
-
-          onClick={() => {
+          className={styles.threeDots}
+          style={{ "--customClickColorChange": moreSettingsClicked ? "#fff" : "#000", "--customHighlight": moreSettingsClicked ? "#000" : "#fff" } as React.CSSProperties} onClick={() => {
             moreSettingsClickedSet(prev => !prev)
           }}
           xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512"><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" /></svg>
