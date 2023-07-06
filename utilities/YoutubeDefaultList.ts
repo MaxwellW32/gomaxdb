9732cf359b332b57d5518f214ddab7eebac99df3
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-07-05 22:38:28 -0500

    cool

--- utilities/YoutubeDefaultList.ts
+++ utilities/YoutubeDefaultList.ts
@@ -28,8 +28,29 @@ const YoutubeDefault = [
     "https://www.youtube.com/watch?v=uAOR6ib95kQ&pp=ygUIZ29yaWxsYXo%3D",
     "https://www.youtube.com/watch?v=HyHNuVaZJ-k&ab_channel=Gorillaz",
     "https://www.youtube.com/watch?v=bvC_0foemLY&ab_channel=RobinSchulz",
-    "https://www.youtube.com/watch?v=C-fexNlzMtQ&ab_channel=RoseateMusic"
-]
+    "https://www.youtube.com/watch?v=C-fexNlzMtQ&ab_channel=RoseateMusic",
+    "https://www.youtube.com/watch?v=wnJ6LuUFpMo&ab_channel=jbalvinVEVO",
+    "https://www.youtube.com/watch?v=RRl_C73vFtQ",
+    "https://www.youtube.com/watch?v=iI34LYmJ1Fs",
+    "https://www.youtube.com/watch?v=ApXoWvfEYVU",
+    "https://www.youtube.com/watch?v=tcaw6lzYt1Q",
+    "https://www.youtube.com/watch?v=LlU4FuIJT2k",
+    "https://www.youtube.com/watch?v=5XK4v2fgMPU",
+    "https://www.youtube.com/watch?v=KBtk5FUeJbk",
+    "https://youtu.be/6ONRf7h3Mdk",
+    "https://youtu.be/34Na4j8AVgA",
+    "https://www.youtube.com/watch?v=r_0JjYUe5jo",
+    "https://www.youtube.com/watch?v=1XzY2ij_vL4",
+    "https://youtu.be/SC4xMk98Pdc",
+    "https://youtu.be/cr82wSBZeeQ",
+    "https://youtu.be/wXhTHyIgQ_U",
+    "https://youtu.be/UZwi9SHgzGY",
+    "https://youtu.be/E5ONTXHS2mM",
+    "https://youtu.be/ZD6C498MB4U",
+    "https://youtu.be/L7mfjvdnPno",
+    "https://www.youtube.com/watch?v=8UVNT4wvIGY",
+    "https://www.youtube.com/watch?v=0bOUOCo6NLQ",
+  ]
 
 const YoutubeDefaultList = (array: any)=> {
     for (let i = array.length - 1; i > 0; i--) {
@@ -39,4 +60,26 @@ const YoutubeDefaultList = (array: any)=> {
     return array;
 }
     
+
+function testforDubplicates(YoutubeDefaultList: string[]){
+  let foundDuplicates = false
+    let count = 1
+
+    YoutubeDefaultList.forEach(eachYtid => {
+        count = 0; // Reset count for each new value
+
+        YoutubeDefaultList.forEach(eachSeenYtidAgain => {
+            if (eachYtid === eachSeenYtidAgain) {
+                count++;
+
+                if (count > 1) {
+                    foundDuplicates = true;
+                    console.log(`dup ${eachYtid}`);
+                    console.log(`${count}`);
+                }
+            }
+        });
+
+    });
+}
 export default YoutubeDefaultList(YoutubeDefault)
